9e7ad81a457f804d24223e18903717f542286a78
Shelly Stuart <shellzy@gmail.com>
2016-07-22 15:24:14 -0400

    undo changes to webpack config

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,6 +1,6 @@
 var config = {
   entry: {
-    path: './react/src/main.js',
+    path: './react/src/main.js'
   },
   output: {
     path: './app/assets/javascripts',
@@ -11,31 +11,18 @@ var config = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react']
-        }
+        loader: 'babel'
       }
     ]
   },
-  resolve: {
-    modulesDirectories: [
-      'node_modules'
-    ],
-    extensions: [
-      "",
-      ".js",
-      ".jsx"
-    ]
-  },
   devtool: 'eval-source-map'
-}
+};
 
 if (process.env.NODE_ENV === 'production') {
   delete config.devtool;
   var webpack = require('webpack');
   config.plugins = [
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' })
+    new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'})
   ];
 }
 
